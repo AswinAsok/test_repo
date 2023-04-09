5a2f97b5c379213eee43393d7a9464884856f7c9
Aswin Asok <aswinasokofficial@gmail.com>
2023-04-09 22:43:47 +0530

    dev:code-updations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>{import.meta.env.VITE_SOME_KEY}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
