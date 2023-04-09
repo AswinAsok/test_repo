9323892a4707ff6378c8175333ceb7f7516607e6
Aswin Asok <aswinasokofficial@gmail.com>
2023-04-09 22:39:24 +0530

    dev:code-updations

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_SOME_KEY) // 123
   return (
     <div className="App">
       <div>
