2a8a76a678eac6c1846d9215f3963ba2da150e67
Aswin Asok <aswinasokofficial@gmail.com>
2023-04-09 22:51:21 +0530

    dev:code-updations

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and savee to test HMR
         </p>
         <p>{import.meta.env.VITE_SOME_KEY}</p>
       </div>
